6b8cd4e26be7da0d5bd29d352bcf80a072a177fc
Chris Rosales <50466291+rosacry@users.noreply.github.com>
2024-05-06 23:30:11 -0400

    Updated model architecture to LSTM for better handling of sequential data.

--- data/model_architecture.py
+++ data/model_architecture.py
@@ -1,22 +1,18 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 class InvestmentModel(nn.Module):
     def __init__(self, input_size=100, hidden_units=64, num_layers=3, dropout=0.3):
         super(InvestmentModel, self).__init__()
-        layers = []
-        
-        # Create hidden layers based on user-defined parameters
-        for _ in range(num_layers):
-            layers.append(nn.Linear(input_size, hidden_units))
-            layers.append(nn.ReLU())
-            layers.append(nn.Dropout(dropout))
-            input_size = hidden_units
-
-        # Output layer
-        layers.append(nn.Linear(hidden_units, 1))
-
-        # Compose the layers into a neural network
-        self.network = nn.Sequential(*layers)
+        self.lstm = nn.LSTM(input_size, hidden_units, num_layers, batch_first=True, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_units, 1)
 
     def forward(self, x):
+        x, _ = self.lstm(x)
+        x = self.dropout(x)
+        x = F.relu(x[:, -1, :])  # Get the output of the last LSTM sequence
+        x = self.fc(x)
+        return x
+
         return self.network(x)
